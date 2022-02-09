0a86f50effb487385d835ae646947048c089ac90
Diego Perillo <sis.dperillo@gmail.com>
2022-02-09 17:14:34 -0300

    Add project files.

---
+++ Log4Net.API/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Logging.SetMinimumLevel(LogLevel.Information)
+               .AddLog4Net("log4net.config");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+    
---
+++ Log4Net.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using log4net;
+using log4net.Config;
+using System.Reflection;
+
+class Program
+{
+    private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType);
+
+    static void Main(string[] args)
+    {
+        var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+        XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+        Console.WriteLine("Hello, World!");
+
+        _log.Info("This is Info");
+        _log.Error("This is Error");
+        _log.Warn("This is Warn");
+        _log.Fatal("This is Fatal");
+        _log.Debug("This is Debug");
+        Console.ReadKey();
+    }
+}
